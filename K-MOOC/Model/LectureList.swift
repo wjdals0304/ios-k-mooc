0858ff41aef9cb942f0335b7e3cd1ac39ea1c451
jmkim <wjdals0304@gmail.com>
2022-04-10 14:25:52 +0900

    add:  Lecture model 파일 추가

---
+++ K-MOOC/Model/LectureList.swift
@@ -0,0 +1,32 @@
+//
+//  LectureList.swift
+//  K-MOOC
+//
+//  Created by 김정민 on 2022/04/10.
+//
+
+import Foundation
+
+struct LectureList: Codable {
+    let count: Int
+    let numPages: Int
+    let previous: String
+    let next: String
+    var lectures: [Lecture]
+
+    init(count: Int,
+         numPages: Int,
+         previous: String,
+         next: String,
+         lectures: [Lecture]) {
+        self.count = count
+        self.numPages = numPages
+        self.previous = previous
+        self.next = next
+        self.lectures = lectures
+    }
+}
+
+extension LectureList {
+    static let EMPTY = LectureList(count: 0, numPages: 0, previous: "", next: "", lectures: [])
+}
