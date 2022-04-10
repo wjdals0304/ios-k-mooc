27ee9add4e583aec6920d1bfd0aa6f584ef0fe1e
jmkim <wjdals0304@gmail.com>
2022-04-10 15:29:03 +0900

    add: url endpoint file add

---
+++ K-MOOC/Network/LectureUrlEndPoint.swift
@@ -0,0 +1,31 @@
+//
+//  LectureUrlEndPoint.swift
+//  K-MOOC
+//
+//  Created by 김정민 on 2022/04/10.
+//
+
+import Foundation
+
+enum LectureUrlEndPoint {
+    case courseList(serviceKey: String, mobile: String)
+    case courseDetail(serviceKey: String, courseId: String)
+}
+
+extension LectureUrlEndPoint {
+    var url: URL {
+        switch self {
+        case .courseList(serviceKey: let serviceKey, mobile: let mobile) :
+            return .makeEndpoint("/courseList?serviceKey=\(serviceKey)&Mobile=\(mobile)")
+        case .courseDetail(serviceKey: let serviceKey, courseId: let courseId):
+            return .makeEndpoint("/courseDetail?servicekey=\(serviceKey)&CourseId=\(courseId)")
+        }
+    }
+}
+
+extension URL {
+    static let baseURL = "http://apis.data.go.kr/B552881/kmooc"
+    static func makeEndpoint(_ endpoint: String) -> URL {
+        return URL(string: baseURL + endpoint)!
+    }
+}
