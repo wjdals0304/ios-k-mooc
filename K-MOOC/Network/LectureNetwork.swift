9b1179e81c444aedfc3d665c369befc1343b0e98
jmkim <wjdals0304@gmail.com>
2022-04-12 22:38:07 +0900

    add: lectureLIst, lecture 파싱

--- K-MOOC/Network/LectureNetwork.swift
+++ K-MOOC/Network/LectureNetwork.swift
@@ -12,41 +12,59 @@ import Alamofire
 final class LectureNetwork {
     
     private let serviceKey = "LwG%2BoHC0C5JRfLyvNtKkR94KYuT2QYNXOT5ONKk65iVxzMXLHF7SMWcuDqKMnT%2BfSMP61nqqh6Nj7cloXRQXLA%3D%3D"
+        
     
     func getLectureList(completion: @escaping(Result<LectureList,APIError>) -> Void) {
         
+        let url = LectureUrlEndPoint.courseList(serviceKey: serviceKey, mobile: String(1)).url
+
+        let dataRequest = AF.request(url, method: .get)
         
-        let url = LectureUrlEndPoint.courseList(serviceKey: serviceKey, mobile: String(1))
-        
-        let dataRequest = AF.request(url as! URLConvertible,method: .get)
-        
-        dataRequest.responseDecodable(of:LectureList.self) { response in
+        dataRequest.responseData { response in
             
             switch response.result {
                 
-            case .success(let result):
+            case .success:
                 
                 guard let statusCode = response.response?.statusCode else {
                     completion(.failure(.failed))
                     return
                 }
                 
+                guard let value = response.value else {
+                    completion(.failure(.noData))
+                    return
+                }
+                
                 switch statusCode {
                     
                 case 200 :
-                    completion(.success(result))
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    guard let data = try? decoder.decode(LectureListResponseModel.self, from: value )
+                    else {
+                        completion(.failure(.invalidData))
+                        return
+                    }
+                    
+                    let lectures = data.results.map { res in
+                        Lecture(id: res.id, number: res.number, name: res.name, classfyName: res.classfyName, middleClassfyName: res.middleClassfyName ?? "", courseImage: res.media.courseImage.uri ?? "" , courseImageLarge: res.media.image.large, shortDescription: res.shortDescription, orgName: res.orgName, start: res.start, end: res.end, teachers: res.teachers, overview: "")
+                    }
+
+                    let lectureList = LectureList(count: data.pagination.count, numPages: data.pagination.numPages, previous: data.pagination.previous ?? "", next: data.pagination.next, lectures: lectures)
+                    
+                    
+                    completion(.success(lectureList))
                 case 403 :
                     completion(.failure(.forbidden))
                 case 500 :
                     completion(.failure(.serverError))
                 default :
                     completion(.failure(.failed))
-                    
                 }
             
-            case .failure :
+            case .failure(let error):
                 completion(.failure(.failed))
-
             }
         }
         
@@ -58,3 +76,4 @@ final class LectureNetwork {
     
     
 }
+
