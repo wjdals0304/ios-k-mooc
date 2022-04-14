da04a5a19b02061796323de52ba1fe3d12330a90
jmkim <wjdals0304@gmail.com>
2022-04-14 11:54:43 +0900

    add: cell UI 레이아웃 추가 및 데이터 바인딩  함수 추가

--- K-MOOC/Presentation/List/KmoocListItemTableViewCell.swift
+++ K-MOOC/Presentation/List/KmoocListItemTableViewCell.swift
@@ -5,20 +5,92 @@
 //  Created by 김정민 on 2022/04/09.
 //
 import UIKit
+import Kingfisher
 
 final class KmoocListItemTableViewCell: UITableViewCell {
-    
+
     static let cellIdentifier = String(describing: KmoocListItemTableViewCell.self)
-    
+
+    private let horizontalStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 16
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    private let verticalStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 0
+        stackView.axis = .vertical
+        return stackView
+    }()
+
     private let thumbnail = UIImageView()
-    private let nameLabel = UILabel()
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+
     private let orgNameLabel = UILabel()
-    private let duration = UILabel()
-    
-    
-    func setLecture( ) {
-        
-        
+    private let durationLabel = UILabel()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+        setupConstraints()
     }
-    
+
+    func setup() {
+
+        [
+          horizontalStack
+        ].forEach { addSubview($0) }
+
+        [
+          thumbnail,
+          verticalStack
+        ].forEach {  horizontalStack.addArrangedSubview($0) }
+
+        [
+         nameLabel,
+         orgNameLabel,
+         durationLabel
+        ].forEach { verticalStack.addArrangedSubview($0) }
+
+    }
+
+    func setupConstraints() {
+
+        horizontalStack.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        thumbnail.setContentHuggingPriority(.init(rawValue: 1000), for: .vertical)
+        thumbnail.setContentHuggingPriority(.init(rawValue: 1000), for: .horizontal)
+        thumbnail.snp.makeConstraints { make in
+
+            make.height.equalTo(self.contentView.snp.height)
+            make.width.equalTo(thumbnail.snp.height).multipliedBy(16/9)
+        }
+
+        nameLabel.snp.makeConstraints { make in
+            make.height.equalTo(40)
+        }
+    }
+
+    func setLecture(_ lecture: Lecture) {
+
+        self.nameLabel.text = lecture.name
+        self.orgNameLabel.text = lecture.orgName
+        self.durationLabel.text = DateUtil.dueString(start: lecture.start, end: lecture.end)
+
+        let url = URL(string: lecture.courseImageLarge)
+        self.thumbnail.kf.setImage(with: url)
+    }
+
 }
